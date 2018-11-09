3fb325cf98ae71a09ff8ef1b67c96288fc271250
Renae L Mitchell <oneprincesslea@gmail.com>
2018-11-09 15:57:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-var word; 
-var word = "bird";
-console.log(word);
-
-word = "dog";
-console.log(word);
-
-var a=5, b=2, c=3, d="hello", e="goodbye";
-
-console.log(`Say ${d} and ${e}!`);
+function sayHelloToSofia() {
+  console.log("Hello, Sofia!")
+}
 
 

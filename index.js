5e15548616c7ef7f4359562eb3471c53dfb6c5b2
Renae L Mitchell <oneprincesslea@gmail.com>
2018-11-09 16:00:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+function sayHelloToIsabel() {
+  console.log("Hello, Isabel!")
+}
 function sayHelloToSofia() {
   console.log("Hello, Sofia!")
 }
 
-
+function sayHelloToBrendan() {
+  console.log("Hello, Brendan!")
+}
+sayHelloToIsabel();
+sayHelloToSofia();
+sayHelloToBrendan();
